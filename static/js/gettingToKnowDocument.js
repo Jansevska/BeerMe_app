d318a7008a94212062bd4ad04effed1c315850c2
Emili <ejanchevis@gmail.com>
2023-11-03 05:42:35 -1000

    first commit

---
+++ static/js/gettingToKnowDocument.js
@@ -0,0 +1,94 @@
+console.log("Let's get to know the document!");
+
+console.log(document);
+console.log(typeof document);
+
+let body = document.body;
+console.log(body);
+// console.dir(body);
+
+// Add CSS Inline style via the .style property
+body.style.backgroundColor = 'crimson';
+
+
+let children = body.children
+console.log(children);
+
+let header = children[0];
+console.log(header);
+console.log(header.children);
+
+let navbar = header.children[0];
+console.log(navbar);
+// Change/Set the class of an element using its .className property
+navbar.className = 'navbar bg-primary navbar-expand-lg';
+
+
+
+
+// Popular methods with the document object
+
+// Document GET methods
+
+// document.getElementById('id')
+// return the first element with an id that matches string 'id'
+let homeElement = document.getElementById('hone');
+console.log(homeElement);
+
+// document.getElementsByTagName('tagName')
+// return HTMLCollection (Array-like) of all elements that match 'tagName'
+let allImages = document.getElementsByTagName('img');
+console.log(allImages);
+
+
+// document.getElementsByClassName('className')
+// return HTMLCollection (Array-like) of all elements that match 'className'
+let invisibleDivs = document.getElementsByClassName('is-invisible');
+console.log(visibleDivs);
+
+
+// document.querySelector('selector')
+// return the FIRST element that matches the specified 'selector'
+let firstNavItem = document.querySelector('.nav-item') // simple selector - class selector
+console.log(firstNavItem);
+
+let middleBubble = document.querySelector('#outerBubble div') // Combinator Selector - descendant
+console.log(middleBubble);
+
+
+// document.querySelectorAll('selector')
+// return a NodeList (Array-like) of elements that match the specified 'selector'
+let navItems = document.querySelectorAll('.nav-item');
+console.log(navItems);
+
+
+
+// Create elements with the document
+
+// document.createElement('tagName')
+// Creaete a new element with the given tag name
+let newHeader = document.createElement('h3');
+newHeader.innerHTML = 'Header Created by Brian with the help of JavaScript';
+newHeader.className = 'text-center text-danger';
+console.log(newHeader);
+
+// Add the element to the HTML document
+
+let heroText = document.getElementsByClassName('hero-text')[0];
+// console.log(heroText);
+
+// Element.append(elementToAdd)
+// Append the elementToAdd as the LAST CHILD of Element
+// heroText.append(newHeader);
+
+// Element.prepend(elementToAdd)
+// Prepend the elementToAdd as the FIRST CHILD of Element
+// heroText.prepend(newHeader);
+
+// Element.after(elementToAdd)
+// Add the elementToAdd AFTER (as a sibling) the Element
+// heroText.after(newHeader);
+
+// Element.before(elementToAdd)
+// Add the elementToAdd BEFORE (as a sibling) the Element
+// heroText.before(newHeader);
---
+++ static/js/index.js
@@ -0,0 +1,219 @@
+console.log('Test is working?') // check live preview -> console
+pageLoader();
+
+// Function to load the page and set event listeners
+function pageLoader(){
+    console.log('Loading the page with functionality...')
+
+    // Get the color buttons to change background
+    const colorButtons = document.getElementsByClassName('light-dark-button');
+    for (let btn of colorButtons){
+        btn.addEventListener('click', changeBackgroundColor)
+    }
+
+    // Get the nav links and add the changeView event listener
+    const navLinks = document.getElementsByClassName('nav-link');
+    for (let link of navLinks){
+        link.addEventListener('click', changeView)
+    }
+
+    // Add the brew finder when the form submits
+    const findBrewsForm = document.querySelector('#find-brews-form');
+    findBrewsForm.addEventListener('submit', e => findBreweries(e, 1));
+
+    // Add drag and drop for the beer and coaster
+    let coasterDrop = document.getElementById('droppable');
+    coasterDrop.addEventListener('dragover', allowDrop);
+    coasterDrop.addEventListener('drop', handleDrop);
+    let draggableBeer = document.getElementById('draggable');
+    draggableBeer.addEventListener('dragstart', dragBeer);
+
+    // Add click listeners to the Bubbles to show event propagation
+    const bubbles = document.getElementsByClassName('bubble');
+    bubbles[0].addEventListener('click', e => console.log("You clicked outer... propagated from: ", e.target.id));
+    bubbles[1].addEventListener('click', e => {
+        console.log("You clicked middle... propagated from: ", e.target.id);
+        // e.stopPropagation(); // This will stop the event from also calling the outer event
+    });
+    bubbles[2].addEventListener('click', e => console.log("You clicked inner... propagated from: ", e.target.id));
+}
+
+
+// Create a function that will change the background color
+function changeBackgroundColor(e){
+    console.log('Clicked Color Button');
+    console.log(e.target.value);
+    if (e.target.value === 'Dark'){
+        document.body.style.backgroundColor = '#C96E12'
+    } else {
+        document.body.style.backgroundColor = '#FFF897'
+    }
+}
+
+
+// Create a function to make this a Single Page App (SPA) by swapping visible divs
+function changeView(e){
+    // Turn off the element(s) that are visible
+    const toTurnOff = document.getElementsByClassName('is-visible');
+    for (let element of toTurnOff){
+        element.classList.replace('is-visible', 'is-invisible');
+        let navLink = document.getElementsByName(element.id)[0];
+        navLink.classList.remove('active');
+    }
+    // Turn on the element based on the link we clicked
+    let idToTurnOn = e.target.name;
+    const toTurnOn = document.getElementById(idToTurnOn);
+    toTurnOn.classList.replace('is-invisible', 'is-visible');
+    e.target.classList.add('active');
+
+    // If the view is the grab beer, listen for keydown
+    if (idToTurnOn === 'grab'){
+        document.addEventListener('keydown', handleBeerMove);
+    } else {
+        document.removeEventListener('keydown', handleBeerMove);
+    }
+}
+
+
+// Function to get brewery data
+function findBreweries(e, pageNumber){
+    e.preventDefault();
+    // Get the value from the city Input
+    const cityName = document.getElementsByName('city')[0].value;
+    console.log(`Looking for breweries in ${cityName}...`);
+
+    const url = `https://api.openbrewerydb.org/v1/breweries?by_city=${cityName}&per_page=10&page=${pageNumber}`
+    console.log(url);
+
+    // Make API request with city name
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayBreweries(data, pageNumber))
+        .catch(err => console.error(err))
+
+}
+
+
+// Callback function for findBreweries that will insert breweries into the table
+function displayBreweries(data, pageNumber){
+    let table = document.getElementById('brewery-table');
+
+    // Clear out the table of any current data
+    clearTable(table);
+
+    // Create the brewery table headers
+    const thead = document.createElement('thead');
+    table.append(thead);
+    let tr = document.createElement('tr');
+    thead.append(tr);
+    const tableHeadings = ['Name', 'Type', 'Street Address', 'Address 2', 'Address 3', 'City', 'State'];
+    tableHeadings.forEach( heading => {
+        let th = document.createElement('th');
+        th.scope = 'col';
+        th.innerHTML = heading;
+        tr.append(th)
+    })
+
+    let tbody = document.createElement('tbody');
+    table.append(tbody);
+    // write a row for each brewery in data
+    for (let brewery of data){
+        let tr = document.createElement('tr');
+        tbody.append(tr);
+
+        newDataCell(tr, `<a href=${brewery.website_url} target="_blank">${brewery.name}</a>`);
+        newDataCell(tr, brewery.brewery_type);
+        newDataCell(tr, brewery.street);
+        newDataCell(tr, brewery.address_2);
+        newDataCell(tr, brewery.address_3);
+        newDataCell(tr, brewery.city);
+        newDataCell(tr, brewery.state);
+    }
+
+        // Add a next button if there is data
+        if (data.length === 10){
+            let nextButton = document.createElement('button');
+            nextButton.classList.add('prev-next-btn', 'btn', 'btn-primary');
+            nextButton.innerHTML = 'Next';
+            nextButton.addEventListener('click', e => findBreweries(e, pageNumber + 1));
+            table.after(nextButton);
+        }
+    
+        // Add a previous button for all pages page 1
+        if (pageNumber > 1){
+            let prevButton = document.createElement('button');
+            prevButton.classList.add('prev-next-btn', 'btn', 'btn-danger');
+            prevButton.innerHTML = 'Prev';
+            prevButton.addEventListener('click', e => findBreweries(e, pageNumber - 1))
+            table.after(prevButton);
+        }
+    }
+
+    // Helper Function to create a new data cell for the table
+function newDataCell(tr, value){
+    let td = document.createElement('td');
+    td.innerHTML = value ?? '-';
+    tr.append(td)
+}
+
+// Helper Function to clear the table and buttons
+function clearTable(table){
+    table.innerHTML = '';
+    const buttonsToClear = document.querySelectorAll('.prev-next-btn');
+    for (let btn of buttonsToClear){
+        btn.remove()
+    }
+}
+
+
+// Function to allow drop events by stopping the default behavior for dragging
+function allowDrop(e){
+    // console.log('Allowing drop on:', e.target);
+    e.preventDefault();
+}
+
+
+// Function to get the Beer ID when the drag starts
+function dragBeer(e){
+    console.log('Dragging beer...');
+    e.dataTransfer.setData('text', e.target.id);
+}
+
+
+// Function to handle the drop
+function handleDrop(e){
+    console.log('Dropping beer');
+    const beerID = e.dataTransfer.getData('text');
+    console.log(beerID);
+    const beer = document.getElementById(beerID);
+    e.target.append(beer);
+}
+
+// Function to handle moving the beer with the arrow keys
+function handleBeerMove(e){
+    console.log(e.key);
+    const arrowKeys = ['ArrowUp', 'ArrowRight', 'ArrowDown', 'ArrowLeft'];
+    // If the user presses one of the arrow keys
+    if (arrowKeys.includes(e.key)){
+        console.log(e.key);
+        const glass = document.querySelector('.beerglass');
+        switch(e.key){
+            case 'ArrowUp':
+                glass.style.top = parseInt(glass.style.top) -5 + 'px';
+                break;
+            case 'ArrowDown':
+                glass.style.top = parseInt(glass.style.top) +5 + 'px';
+                break;
+            case 'ArrowLeft':
+                glass.style.left = parseInt(glass.style.left) -5 + 'px';
+                break;
+            case 'ArrowRight':
+                glass.style.left = parseInt(glass.style.left) +5 + 'px';
+                break;
+        };
+
+        if (glass.style.top === '200px' && glass.style.left ==='450px'){
+            setTimeout(() => alert('Enjoy a nice cold beer!'), 0)
+        }
+    }
+}
